5635cb32450d5aa0b33fef929cb4bf35e91910b9
Edward Taupier <etaupier@cblpath.com>
2011-06-11 14:41:08 -0400

    fix typo

--- lib/generators/simple_lightbox/install_generator.rb
+++ lib/generators/simple_lightbox/install_generator.rb
@@ -5,7 +5,7 @@ module SimpleLightbox
     class_option :template_engine
 
     def copy_files
-      template "simple_lightbox.css", "pubic/stylesheets/simple_lightbox.css"
+      template "simple_lightbox.css", "public/stylesheets/simple_lightbox.css"
       template "simple_lightbox.js", "public/javascripts/simple_lightbox.js"
     end
 
